82ccbbbce1f58edf2cdfd4edaabc77966f696fd0
muhammad-jiku <muhammadjiku364@gmail.com>
2022-04-11 00:42:44 +0600

    first commit

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,22 +1,23 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCk7iTgiGpzXvhjitNRf3Oa1EqkKZqBzNo",
-  authDomain: "ema-john-simple-85c01.firebaseapp.com",
-  projectId: "ema-john-simple-85c01",
-  storageBucket: "ema-john-simple-85c01.appspot.com",
-  messagingSenderId: "598631523012",
-  appId: "1:598631523012:web:16797e70a7e5e9f0acdf91"
+  apiKey: 'AIzaSyCTz1iFoMrarY0pOLbYvPgwOVxhGGtckGo',
+  authDomain: 'jikmunn-ema-john-clone.firebaseapp.com',
+  projectId: 'jikmunn-ema-john-clone',
+  storageBucket: 'jikmunn-ema-john-clone.appspot.com',
+  messagingSenderId: '180853695823',
+  appId: '1:180853695823:web:f7ea48a848db43d210913a',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
+
+// export const auth = getAuth(app);
 
 export default auth;
